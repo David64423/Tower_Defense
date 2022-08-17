f5cbf4468297fff8d55409de736d667f47734aa5
Juan David Lopez <david16115@outlook.com>
2022-08-17 09:55:49 -0300

    Agregando imagenes de ataque

--- public/java/indexStyle.js
+++ public/java/indexStyle.js
@@ -8,7 +8,7 @@ let distanciaX=0;
 let distanciaY=0;    
 let distanciaM=0.7;
 izquierda.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",restarX)});
-derecha.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",sumarX)});
+derecha.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",automatizar)});
 arriba.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",sumarY)});
 abajo.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",restarY)});
 
@@ -28,7 +28,7 @@ function mainCaminar(a,b,c,fnc){
     let tiempo=0;
     let i=0;
 
-    while(i<3){
+    while(i<1){
         if ((distanciaX<=(2) && fnc===restarX) || (distanciaX>=(34) && fnc===sumarX)|| (distanciaY<=(-13) && fnc===sumarY) || (distanciaY>=(32) && fnc===restarY) ){
             
         }
@@ -91,5 +91,18 @@ function cambiar(a,b){
     console.log("Este es el mensaje")
 };
 
+
+
+function automatizar(a,b,c){
+    let tiempo = 0;
+    setTimeout(()=>{
+        tiempo = tiempo+2000;
+    while(1>1){
+        mainCaminar(a,b,c,sumarX);
+    }
+    
+},tiempo)
+
+};
 
 

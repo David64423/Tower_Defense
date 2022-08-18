22b4301f74d457c540b43a31db3ba1da13889272
Juan David Lopez <david16115@outlook.com>
2022-08-17 21:29:02 -0300

    Solucionando errores sin importancia

--- public/java/indexStyle.js
+++ public/java/indexStyle.js
@@ -8,7 +8,7 @@ let distanciaX=0;
 let distanciaY=0;    
 let distanciaM=0.7;
 izquierda.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",restarX)});
-derecha.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",automatizar)});
+derecha.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",sumarX)});
 arriba.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",sumarY)});
 abajo.addEventListener('click',function(){ mainCaminar(personaje2,"pers2_1.png","pers2_2.png",restarY)});
 
@@ -28,7 +28,7 @@ function mainCaminar(a,b,c,fnc){
     let tiempo=0;
     let i=0;
 
-    while(i<1){
+    while(i<3){
         if ((distanciaX<=(2) && fnc===restarX) || (distanciaX>=(34) && fnc===sumarX)|| (distanciaY<=(-13) && fnc===sumarY) || (distanciaY>=(32) && fnc===restarY) ){
             
         }
